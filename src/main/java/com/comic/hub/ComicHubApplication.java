0c4dadc5d452c7c9e0ff4ad3ba8a5825678a44bf
Leonardo474-cpu <leo8minaya@gmail.com>
2026-02-15 11:58:20 -0500

    Inicio del proyecto ComicHub

---
+++ src/main/java/com/comic/hub/ComicHubApplication.java
@@ -0,0 +1,13 @@
+package com.comic.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comic/hub/ComicHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comic.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
