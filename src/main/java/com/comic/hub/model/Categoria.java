eecd52a2a317e121626a873010e0d3f761c6024f
AleMel27 <andremarcasmelgar@gmail.com>
2026-02-18 12:59:30 -0500

    merge

---
+++ src/main/java/com/comic/hub/model/Categoria.java
@@ -0,0 +1,17 @@
+package com.comic.hub.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class Categoria {
+	
+	private int idCategoria;
+	
+	private String descripcion;
+	
+	
+}
